ff54268d0b2710d09b50caf627da9f55a5454354
wubingheng <wubinghengajw@outlook.com>
2019-06-04 22:27:56 +0800

    add AliOssPrivateUrl to sign ali private url.

--- src/main/java/com/qiniu/AliOssPrivateUrl.java
+++ src/main/java/com/qiniu/AliOssPrivateUrl.java
@@ -1,40 +1,83 @@
 package com.qiniu;
 
+import com.aliyun.oss.OSSClient;
 import com.qiniu.interfaces.ILineProcess;
 import com.qiniu.process.Base;
 import com.qiniu.storage.Configuration;
+import com.qiniu.util.OssUtils;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
-public class AliOssPrivateUrl extends Base<Map<String, String >> {
+public class AliOssPrivateUrl extends Base<Map<String, String>> {
 
-    public AliOssPrivateUrl(String accessKey, String secretKey, Configuration configuration, String bucket) throws IOException {
+    private OSSClient ossClient;
+    private String region;
+    private String domain;
+    private String protocol;
+    private String urlIndex;
+    private Date expiration = new Date(new Date().getTime() + 3600 * 1000);
+    private ILineProcess<Map<String, String>> nextProcessor;
+
+    public AliOssPrivateUrl(String accessKey, String secretKey, Configuration configuration, String bucket, String domain)
+            throws IOException {
         super("aliprivate", accessKey, secretKey, configuration, bucket);
+        region = OssUtils.getAliOssRegion(accessKey, secretKey, bucket);
+        ossClient = new OSSClient(region, accessKey, secretKey);
+        this.domain = domain;
+    }
+
+    public AliOssPrivateUrl(String accessKey, String secretKey, Configuration configuration, String bucket, String domain,
+                            String savePath, int saveIndex) throws IOException {
+        super("aliprivate", accessKey, secretKey, configuration, bucket, savePath, saveIndex);
+        region = OssUtils.getAliOssRegion(accessKey, secretKey, bucket);
+        ossClient = new OSSClient(OssUtils.getAliOssRegion(accessKey, secretKey, bucket), accessKey, secretKey);
+        this.domain = domain;
+    }
+
+    public AliOssPrivateUrl(String accessKey, String secretKey, Configuration configuration, String bucket, String domain,
+                            String savePath) throws IOException {
+        super("aliprivate", accessKey, secretKey, configuration, bucket, savePath, 0);
+        region = OssUtils.getAliOssRegion(accessKey, secretKey, bucket);
+        ossClient = new OSSClient(OssUtils.getAliOssRegion(accessKey, secretKey, bucket), accessKey, secretKey);
+        this.domain = domain;
+    }
+
+    public AliOssPrivateUrl clone() throws CloneNotSupportedException {
+        AliOssPrivateUrl aliOssPrivateUrl = (AliOssPrivateUrl)super.clone();
+        aliOssPrivateUrl.ossClient = new OSSClient(region, accessKey, secretKey);
+        return aliOssPrivateUrl;
     }
 
     @Override
     public String resultInfo(Map<String, String> line) {
-        return null;
+        return line.get("key");
     }
 
     @Override
     public boolean validCheck(Map<String, String> line) {
-        return false;
+        return true;
     }
 
     @Override
     public void setNextProcessor(ILineProcess<Map<String, String>> nextProcessor) {
-
-    }
-
-    @Override
-    public ILineProcess<Map<String, String>> getNextProcessor() {
-        return null;
+        this.nextProcessor = nextProcessor;
     }
 
     @Override
     public String singleResult(Map<String, String> line) throws IOException {
-        return null;
+        // 生成以GET方法访问的签名URL，访客可以直接通过浏览器访问相关内容。
+        URL url = ossClient.generatePresignedUrl(bucket, "http://" + domain + "/" + line.get("key"), expiration);
+        if (nextProcessor != null) {
+            Map<String, String> map = new HashMap<String, String>(){{
+                put("key", url.toString());
+            }};
+            return nextProcessor.processLine(map);
+        } else {
+            return url.toString();
+        }
     }
 }
--- src/main/java/com/qiniu/App.java
+++ src/main/java/com/qiniu/App.java
@@ -1,13 +1,34 @@
 package com.qiniu;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.qiniu.config.ParamsConfig;
+import com.qiniu.config.PropertiesFile;
+import com.qiniu.datasource.IDataSource;
+import com.qiniu.entry.CommonParams;
+import com.qiniu.entry.QSuitsEntry;
+import com.qiniu.interfaces.IEntryParam;
+import com.qiniu.interfaces.ILineProcess;
+
+import java.util.Map;
+import java.util.Properties;
+
+public class App {
+
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) throws Exception {
+        Properties properties = new PropertiesFile("src/resources/.config.properties").getProperties();
+        IEntryParam entryParam = new ParamsConfig(properties);
+        QSuitsEntry qSuitsEntry = new QSuitsEntry(entryParam);
+        ILineProcess<Map<String, String>> processor = qSuitsEntry.getProcessor();
+        CommonParams commonParams = qSuitsEntry.getCommonParams();
+        AliOssPrivateUrl aliOssPrivateUrl = new AliOssPrivateUrl(commonParams.getAliyunAccessId(),
+                commonParams.getAliyunAccessSecret(), null, commonParams.getBucket(),
+                entryParam.getValue("domain"), commonParams.getSavePath());
+        aliOssPrivateUrl.setNextProcessor(processor);
+        IDataSource dataSource = qSuitsEntry.getDataSource();
+        if (dataSource != null) {
+            dataSource.setProcessor(aliOssPrivateUrl);
+            dataSource.export();
+        }
+        aliOssPrivateUrl.closeResource();
     }
 }
